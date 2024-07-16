ef3fbd748d72a187b7b5f7cf45f166432c428e7e
Niati <niatiniati545@gmail.com>
2024-07-16 19:34:41 +0700

    beberapa view

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,42 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', function () {
     return view('test');
 });
+
+Route::prefix('components')->group(function () {
+    Route::get('/FooterAdmin', function () {
+        return view('components/footeradmin');
+    });
+
+    Route::get('/FooterUser', function () {
+        return view('components/footeruser');
+    });
+
+    Route::get('/HeaderAdmin', function () {
+        return view('components/headeradmin');
+    });
+
+    Route::get('/HeaderUser', function () {
+        return view('components/headeruser');
+    });
+
+});
+
+Route::prefix('user')->group(function () {
+    Route::get('/', function () {
+        return view('user/home');
+    });
+
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin/home');
+    });
+
+    Route::get('/Novel', function () {
+        return view('admin/novel');
+    });
+
+});
+
+
