0cbe9b2d64fd98301a3819a2a9ef76a0f0e9d272
Niati <niatiniati545@gmail.com>
2024-07-17 18:53:20 +0700

    javascript flowbite

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -16,7 +16,7 @@
             <!-- Carousel wrapper -->
             <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
                 <!-- Item 1 -->
-                <div class="duration-700 ease-in-out" data-carousel-item>
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
                     <img src="{{ url('images/slider.jpg') }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
                 <!-- Item 2 -->
