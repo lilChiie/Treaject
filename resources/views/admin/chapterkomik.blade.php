1e9c5c67bbe5828b0b5e64d98b10010023e192a7
Niati <niatiniati545@gmail.com>
2024-07-22 14:26:58 +0700

    komik v2

--- resources/views/admin/chapterkomik.blade.php
+++ resources/views/admin/chapterkomik.blade.php
@@ -31,27 +31,28 @@
     <!-- panah start -->
     <section>
         <div class="flex justify-center mt-5">
-            <a href="">
+            <a href="" class=" hover:text-merah rounded-xl group py-1 transition duration-700">
                 <div class="flex mx-5">
-                    <svg class="w-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                        <path fill="#ababab" d="M459.5 440.6c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29l0-320c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4L288 214.3l0 41.7 0 41.7L459.5 440.6zM256 352l0-96 0-128 0-32c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4l-192 160C4.2 237.5 0 246.5 0 256s4.2 18.5 11.5 24.6l192 160c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29l0-64z"/>
+                    <svg class="w-7 group-hover:fill-merah" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                        <path d="M459.5 440.6c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29l0-320c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4L288 214.3l0 41.7 0 41.7L459.5 440.6zM256 352l0-96 0-128 0-32c0-12.4-7.2-23.7-18.4-29s-24.5-3.6-34.1 4.4l-192 160C4.2 237.5 0 246.5 0 256s4.2 18.5 11.5 24.6l192 160c9.5 7.9 22.8 9.7 34.1 4.4s18.4-16.6 18.4-29l0-64z"/>
                     </svg>
-                    <p class=" mx-4 text-gray-500 uppercase">previous</p>
+                    <p class="mx-4 font-bold text-gray-600 uppercase group-hover:text-merah">previous
+                    </p>
                 </div>
             </a>
 
             <div>
-                <p class=" font-medium text-[18px] mx-6">#1</p>
+                <p class=" font-medium text-[18px] mx-6 py-1">#1</p>
             </div>
 
-            <a href="" class="hover:bg-merah hover:text-white rounded-xl group">
-  <div class="flex mx-5">
-    <p class="mx-4 text-gray-600 uppercase group-hover:text-white">NEXT</p>
-    <svg class="w-7 group-hover:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path fill="#ababab" d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416L0 96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4L224 214.3l0 41.7 0 41.7L52.5 440.6zM256 352l0-96 0-128 0-32c0-12.4 7.2-23.7 18.4-29s24.5-3.6 34.1 4.4l192 160c7.3 6.1 11.5 15.1 11.5 24.6s-4.2 18.5-11.5 24.6l-192 160c-9.5 7.9-22.8 9.7-34.1 4.4s-18.4-16.6-18.4-29l0-64z"/>
-    </svg>
-  </div>
-</a>
+            <a href="" class=" hover:text-merah rounded-xl group py-1  transition duration-700">
+                <div class="flex mx-5">
+                    <p class="mx-4 font-bold text-gray-600 uppercase group-hover:text-merah">NEXT</p>
+                    <svg class="w-7 group-hover:fill-merah" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M52.5 440.6c-9.5 7.9-22.8 9.7-34.1 4.4S0 428.4 0 416L0 96C0 83.6 7.2 72.3 18.4 67s24.5-3.6 34.1 4.4L224 214.3l0 41.7 0 41.7L52.5 440.6zM256 352l0-96 0-128 0-32c0-12.4 7.2-23.7 18.4-29s24.5-3.6 34.1 4.4l192 160c7.3 6.1 11.5 15.1 11.5 24.6s-4.2 18.5-11.5 24.6l-192 160c-9.5 7.9-22.8 9.7-34.1 4.4s-18.4-16.6-18.4-29l0-64z"/>
+                    </svg>
+                </div>
+            </a>
 
         </div>
     </section>
