fb0f93e517ff1e1207548dab22eba41f6a1d8ce9
Niati <niatiniati545@gmail.com>
2024-07-22 17:15:26 +0700

    coment komik balasan

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,14 @@
 import './bootstrap';
 import 'flowbite';
 
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.toggle-replies').forEach(button => {
+        button.addEventListener('click', function(event) {
+            event.preventDefault();
+            const target = document.getElementById(this.dataset.target);
+            if (target) {
+                target.classList.toggle('hidden');
+            }
+        });
+    });
+});
