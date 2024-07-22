dfe268651823557dd38475fda90200aaec670635
shifa humaira <xjieche@gmail.com>
2024-07-22 14:25:01 +0700

    user lagi

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -4,6 +4,7 @@
 <head>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Home | Treaject</title>
    @vite(['resources/css/app.css','resources/js/app.js'])
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-RpIhZ+1fUwzGZk6gHf41rUzYmiHPc4E8vKscIiV3j8KTVt+GVGeNcLXZIuM3Nq5vB+RREoCg5NZhIcRktrQ3uA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
